8bdc49c4b0fb1d186b24ea6f7b67f198983be2cc
LakshmiNarayana <PerlaLakshmi.Narayana@cognizant.com>
2023-07-06 15:49:43 +0530

    validations for registration form done

--- blog-site-application/src/app/registration/registration.component.ts
+++ blog-site-application/src/app/registration/registration.component.ts
@@ -11,22 +11,76 @@ import { Injectable } from '@angular/core';
 export class RegistrationComponent implements OnInit {
   message : string = ''
   errorMessage : string = ''
+  userNameValidation : string = ''
+  eMailValidation : string = ''
+  passwordValidation : string = ''
   user : User = new User()
   constructor(private _router: Router, private _auth: AuthService) { }  
 
-  RegisterNewUser(){    
-    this.user.Id = 0;
-    this._auth.registerUser(this.user).subscribe(res => {
-      if(res == "true"){
-        this.message = "User registered successfully";
-        this.errorMessage = '';
-        this.user = new User;
+  RegisterNewUser(){ 
+    this.resetValidations();
+    if(this.user.UserName != '' && this.user.Email != '' && this.user.Password != '')
+    {   
+      if(this.user.Email.includes('@') && this.user.Email.endsWith('.com') && 
+         this.user.Password.length >= 8)
+      {
+        this.user.Id = 0;
+        this._auth.registerUser(this.user).subscribe(res => {
+          if(res == "true"){
+            this.successfulRegistration();
+          }
+          else{
+            this.failedRegistration();
+          }
+        });
       }
       else{
-        this.message = "";
-        this.errorMessage = "Username or Email already exists";
+        this.validateEmailAndpasssword();
       }
-      });
+    }
+    else{
+      this.validateRequiredFields();
+    }
+  }
+
+  resetValidations(){
+    this.userNameValidation = ''
+    this.eMailValidation = ''
+    this.passwordValidation = ''
+    this.errorMessage = '';
+    this.message = '';
+  }
+
+  successfulRegistration(){
+    this.message = "User registered successfully";
+    this.errorMessage = '';
+    this.user = new User;
+  }
+
+  failedRegistration(){
+    this.message = '';
+    this.errorMessage = "Username or Email already exists";
+  }
+
+  validateEmailAndpasssword(){
+    if(!(this.user.Email.includes('@') && this.user.Email.endsWith('.com'))){
+      this.eMailValidation = 'Invalid Email';
+    }
+    if(this.user.Password.length < 8){
+      this.passwordValidation = 'Password should be atleast 8 characters';
+    }
+  }
+
+  validateRequiredFields(){
+    if(this.user.UserName == ''){
+      this.userNameValidation = 'Username is required';
+    }
+    if(this.user.Email == ''){
+      this.eMailValidation = 'Email is required';
+    }
+    if(this.user.Password == ''){
+      this.passwordValidation = 'Password is required';
+    }
   }
 
   ngOnInit(): void {        
