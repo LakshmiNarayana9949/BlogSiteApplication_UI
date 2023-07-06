9bdf8c71dcf696f48aca5330e59b503f0e9fa72e
LakshmiNarayana <PerlaLakshmi.Narayana@cognizant.com>
2023-07-06 16:14:16 +0530

    Validation implemented for login

--- blog-site-application/src/app/login/login.component.ts
+++ blog-site-application/src/app/login/login.component.ts
@@ -10,17 +10,40 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   loginDetails : LoginDetails = new LoginDetails();
   errorMessage : string = ''
+  eMailValidation : string = ''
+  passwordValidation : string = ''
   constructor(private router : Router, private auth : AuthService) { }
 
-  Login(){    
-    this.auth.loginUser(this.loginDetails).subscribe(res => { 
-      localStorage.setItem('token', res.RefreshToken);
-      localStorage.setItem('userId', res.Id);    
-      this.router.navigate(['/blog']);     
-    },
-    err => {
-      this.errorMessage = err.error.text;
-    });
+  Login(){ 
+    this.resetValidations(); 
+    if(this.loginDetails.Email != '' && this.loginDetails.Password != '')
+    {  
+      this.auth.loginUser(this.loginDetails).subscribe(res => { 
+        localStorage.setItem('token', res.RefreshToken);
+        localStorage.setItem('userId', res.Id);    
+        this.router.navigate(['/blog']);     
+      },
+      err => {
+        this.errorMessage = err.error.text;
+      });
+    }
+    else{
+      this.validateLoginDetails();
+    }
+  }
+
+  resetValidations(){
+    this.eMailValidation = '';
+    this.passwordValidation = ''
+;  }
+
+  validateLoginDetails(){
+    if(this.loginDetails.Email == ''){
+      this.eMailValidation = 'Email is required';
+    }
+    if(this.loginDetails.Password == ''){
+      this.passwordValidation = 'Password is required';
+    }
   }
   ngOnInit(): void {
   }
