b4cfcb69345051e3ff84aa4f2dc40013667d69e6
LakshmiNarayana <PerlaLakshmi.Narayana@cognizant.com>
2023-07-12 12:55:11 +0530

    validtions implemented for adding a new blog

--- blog-site-application/src/app/addnewblog/addnewblog.component.ts
+++ blog-site-application/src/app/addnewblog/addnewblog.component.ts
@@ -18,11 +18,54 @@ export class AddnewblogComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  AddNewBlog(){    
-    this.blog.CreatedBy = Number(localStorage.getItem('userId'));
-    this.auth.addNewBlog(this.blog).subscribe(res => {
-      debugger;
-    });
+  AddNewBlog(){  
+    this.resetValidations();
+    if(this.blog.BlogName != '' && this.blog.Category != '' && this.blog.Article != ''){
+      if(this.blog.BlogName.length >= 20 && this.blog.Category.length >= 20 && 
+                                            this.blog.Article.length >= 2000){      
+        this.blog.CreatedBy = Number(localStorage.getItem('userId'));
+        this.auth.addNewBlog(this.blog).subscribe(res => {  
+          debugger;
+          this.successMessage = res;
+          this.blog = new Blog();    
+        });
+      }
+      else{
+        this.validateLengths();
+      }
+    }
+    else{
+      this.validateRequiredFields();
+    }
   }
 
+  validateRequiredFields(){
+    if(this.blog.BlogName == ''){
+      this.blogNameValidation = 'Blog name is required';
+    }
+    if(this.blog.Category == ''){
+      this.blogCategoryValidation = 'Category is required';
+    }
+    if(this.blog.Article == ''){
+      this.blogArticleValidation = 'Article is required';
+    }
+  }
+
+  validateLengths(){
+    if(this.blog.BlogName.length < 20){
+      this.blogNameValidation = 'Blog name should be atleast 20 characters';
+    }
+    if(this.blog.Category.length < 20){
+      this.blogCategoryValidation = 'Category should be atleast 20 characters';
+    }
+    if(this.blog.Article.length < 2000){
+      this.blogArticleValidation = 'Article  should be atleast 2000 characters';
+    }
+  }
+
+  resetValidations(){
+    this.blogNameValidation = '';
+    this.blogCategoryValidation = '';
+    this.blogArticleValidation = '';
+  }
 }
