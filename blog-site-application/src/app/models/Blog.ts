1e11fd859827f5366ef86699a1b7e6332e5eacd5
LakshmiNarayana <PerlaLakshmi.Narayana@cognizant.com>
2023-07-19 13:17:20 +0530

    design for view blog

--- blog-site-application/src/app/models/Blog.ts
+++ blog-site-application/src/app/models/Blog.ts
@@ -4,6 +4,7 @@ export class Blog{
     category : string = ''
     article : string = ''
     author : string = ''
+    email : string = ''
     createdBy : number = 0
     createdOn : Date = new Date()   
 }
